c5358761bdc04a690e6d0e7dfe2d599816c12944
ismaelaguayob <ismaelaguayob@gmail.com>
2025-10-12 21:01:10 -0300

    Visualizaciones e invarianza terminadas y métodos corregidos

--- processing/3-cnt-comparability.R
+++ processing/3-cnt-comparability.R
@@ -19,9 +19,31 @@ icils <- icils %>%
 # Verificar los cambios
 unique(icils$CNTRY)
 
+# Aplicamos labels de PISA a ICILS country iso code
+
 paises_comunes <- intersect(unique(pisa$CNT), unique(icils$CNTRY))
 icils_filtrado <- icils %>% filter(CNTRY %in% paises_comunes)
 pisa_filtrado <- pisa %>% filter(CNT %in% paises_comunes)
+
+country_labels <- pisa_filtrado %>%
+  select(CNT) %>%
+  distinct() %>%
+  mutate(
+    code = as.character(CNT),
+    label = as.character(haven::as_factor(CNT))
+  ) %>%
+  select(code, label)
+
+icils_filtrado <- icils_filtrado %>%
+  mutate(
+    CNTRY = haven::labelled(
+      as.character(CNTRY),
+      labels = setNames(country_labels$code, country_labels$label)
+    )
+  )
+
+print(head(haven::as_factor(icils_filtrado$CNTRY)))
+
 
 saveRDS(icils_filtrado, "./input/data/proc_data/icils_comparable.rds")
 saveRDS(pisa_filtrado, "./input/data/proc_data/pisa_comparable.rds")
